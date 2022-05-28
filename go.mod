2f3bf859ce0c632d74b9164f61d6d80c6e7353a5
Sergey <serj_bibox@mail.ru>
2022-05-28 15:18:41 +0600

    first commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module AliceService
+
+go 1.18
+
+require (
+	github.com/go-chi/chi/v5 v5.0.7
+	github.com/go-chi/render v1.0.1
+)
---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
+)
+
+type ResponseInterface interface {
+	Send(w http.ResponseWriter)
+}
+
+func SendHttp(w http.ResponseWriter, v ResponseInterface) {
+	v.Send(w)
+}
+
+// @Description Структура HTTP ответа метода GET /submitData/{id}/status
+type StatusResponse struct {
+	ID     string `json:"id" example:"123"`
+	Status string `json:"status" example:"new"`
+}
+
+func (s StatusResponse) Send(w http.ResponseWriter) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(&s)
+}
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.URLFormat)
+	r.Use(render.SetContentType(render.ContentTypeJSON))
+	r.Get("/", Hello)
+	httpPort := ":"
+	//Чтение системной переменной PORT для деплоя на Heroku
+	if env, ok := os.LookupEnv("PORT"); !ok {
+		httpPort += "8080"
+	} else {
+		httpPort += env
+	}
+	log.Panic(http.ListenAndServe(httpPort, r))
+}
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+	sr := StatusResponse{"1", "new"}
+	SendHttp(w, sr)
+}
